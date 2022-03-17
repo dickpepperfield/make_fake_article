f03a39da703cf2ad33015931eea2d743fe874788
pepperfield <pepperfield@git>
2022-03-17 22:11:42 +1100

    QOL changes

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # make_fake_article
+
 Make a fake article adn post it to a wordpress site
 
 # Prerequisites
+
 Python 3.8+
 python-venv
 Wordpress
@@ -10,26 +12,51 @@ SMTP Server
 
 # Installation
 
-Clone the repo 
+Clone the repo
+
 ```bash
 git clone https://github.com/dickpepperfield/make_fake_article
 ```
 
 Create the virtual environment
+
 ```bash
-python -m venv make_fake_article
+python3 -m venv make_fake_article
 ```
 
 Enter the virtual environment and install requisite packages
+
 ```bash
 cd make_fake_article
 source bin/activate
-pip install requirements.txt
+pip install -r requirements.txt
 ```
 
 Correct .env file with required values
+
 ```bash
 cp env.example .env
 vim .env
 ```
 
+# Usage
+
+Simple commmand usage
+
+```bash
+# With defaults
+python make_fake_article.py
+
+# With modifiers
+python make_fake_article.py --feed https://www.zdnet.com/au/rss.xml --category Technology
+```
+
+Example feeds have been provided in `example_feeds.txt`
+
+First run through may take a while as it downloads the GPT2 dataset.
+
+# TODOs
+
+- Split the single file into a proper python application, with module functions.
+- add more categories, and rss feeds
+- GPU processing
--- make_fake_article.py
+++ make_fake_article.py
@@ -15,8 +15,10 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 
+# Load environment vars
 load_dotenv()
 
+# Vars found in .env
 RSS_FEED = os.getenv('RSS_FEED')
 UNSPLASH_API_KEY = os.getenv('UNSPLASH_API_KEY')
 EMAIL_SMTP_SERVER = os.getenv('EMAIL_SMTP_SERVER')
@@ -27,42 +29,54 @@ EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
 GPT_SEED = random.randint(0,100)
 GPT_MAX_LEN = random.randint(700,900)
 
+# Function to provide user inputs and output userargs
 def userArguments():
     parser = argparse.ArgumentParser(description='Generate a fake news article and post it.')
     parser.add_argument('-f', '--feed', default=RSS_FEED)
     parser.add_argument('-c', '--category', default='Technology')
     parser.add_argument('-l', '--max-length', default=GPT_MAX_LEN)
-
     userargs = parser.parse_args()
-
     return userargs
 
-
+# Function to load inputs from --feed, 
+# Fetches titles from an RSS feed and dumps them into tmp/feeds 
+# then randomly selects one
 def loadFeedTitles(userArguments):
+    # Grab the feed
     feed = feedparser.parse(userArguments.feed)
-
+    # Create a list on disk from all titles
     with open('tmp/feeds', 'w') as f:
         for item in feed.entries:
             print(item[ "title" ], file=f)   
 
+    # Randomly select one
     random_title = random.choice(list(open('tmp/feeds')))
 
     return random_title
 
+# Function to grab an image from Unsplash.com, using the --category userarg as a search term
 def getImage(userArguments):
+    # Connect to API
     pu = pyunsplash.PyUnsplash(api_key=UNSPLASH_API_KEY)
+    # Query API for single image
     photos = pu.photos(type_='random', count=1, featured=True, query=userArguments.category)
     [photo] = photos.entries  
+    # Save response to disk
     response = requests.get(photo.link_download, allow_redirects=True)
     open('tmp/unsplash_temp.png', 'wb').write(response.content)
 
+# Function to create the text, input text that the article is built from is provided from 
+# random_title, out of the tmp/feeds list
 def createGPT2Text():
+    # Grab a random title
     random_title = random.choice(list(open('tmp/feeds')))
+
+    # GPT2 Stuff
     tokenizer = GPT2Tokenizer.from_pretrained("gpt2-large")
     GPT2 = TFGPT2LMHeadModel.from_pretrained("gpt2-large", pad_token_id=tokenizer.eos_token_id)
+    # Input is random title from above, generate text using tensorflow
     input_sequence = random_title
     input_ids = tokenizer.encode(input_sequence, return_tensors='tf')
-    
     sample_output = GPT2.generate(
                              input_ids,
                              do_sample = True,
@@ -70,18 +84,22 @@ def createGPT2Text():
                              top_k = 0,
                              temperature = 0.8
     )
-
+    # Output here
     GPT2Output = tokenizer.decode(sample_output[0], skip_special_tokens = True)
 
     return GPT2Output
 
+# Function to construct the email, attach the article and image, and then send it to 
+# post to email. Has a few hard coded shortcodes that could be ripped out for userargs 
+# and sensible defaults.
 def constructAndSendEmail(userArguments,loadFeedTitles,createGPT2Text):
+    # Basic email properties
     message = MIMEMultipart("alternative")
     message["Subject"] = loadFeedTitles
     message["From"] = EMAIL_SENDER
     message["To"] = EMAIL_RECEIVER
 
-    # Create the plain-text and HTML version of your message
+    # Create the plain-text version of the message
     text = (createGPT2Text + f"""
             [tags Daily, News, Fake, {userArguments.category}] [category {userArguments.category}]
             ALL INFORMATION IN THIS POST IS COMPLETELY FAKE AND GENERATED ON THE FLY \ 
@@ -110,17 +128,13 @@ def constructAndSendEmail(userArguments,loadFeedTitles,createGPT2Text):
             EMAIL_SENDER, EMAIL_RECEIVER, message.as_string()
         )
 
+# Main function
 def main():
     userArguments()
     uargs = userArguments()
-
     loadFeedTitles(uargs)
-
     createGPT2Text()
-
     getImage(uargs)
-
-    # Construct the email
     title = loadFeedTitles(uargs)
     content = createGPT2Text()
     constructAndSendEmail(uargs, title, content)
